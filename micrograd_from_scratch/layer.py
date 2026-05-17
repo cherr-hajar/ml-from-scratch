cad23ce25c9e3ca06219a72ad37d22734e5b54f8
Cherrouk Hajar <hajarcrk26@gmail.com>
2026-05-17 17:24:34 +0100

    implement Layer class

---
+++ micrograd_from_scratch/layer.py
@@ -0,0 +1,11 @@
+from neuron import Neuron
+from value_class import Value
+class Layer():
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x):
+        return [neuron(x) for neuron in self.neurons]
+layer = Layer(2, 3)
+x = [Value(1.0), Value(0.5)]
+print(layer(x))
