5c425f7bac25870f0fc4cf34b001fa156cd80450
Cherrouk Hajar <hajarcrk26@gmail.com>
2026-05-18 19:48:10 +0100

    added some clarifying comments

--- micrograd_from_scratch/mlp.py
+++ micrograd_from_scratch/mlp.py
@@ -2,11 +2,14 @@ from neuron import Neuron
 from layer import Layer
 from value_class import Value
 class MLP():
+    #nin is number of inputs and nout is list of layer sizes 
     def __init__(self, nin, nouts):
-        sizes = [nin] + nouts
+        sizes = [nin] + nouts # all sizes combined 
+        # list of layer objectss built from thsoe sizes 
         self.layers = [Layer(nin, nout) for nin, nout in zip(sizes, sizes[1:])]
     
     def __call__(self, x):
+        #input data gets replaced by each layers output as it passes through
         for layer in self.layers:
             x = layer(x)
         return x
